9fd73b50fbdf986ddfff7eba2df716f3c606dd6a
xzj <xzj007@gmail.com>
2012-02-12 17:52:31 +0800

    add comment to dkj.bat

--- dkj.bat
+++ dkj.bat
@@ -1,6 +1,9 @@
 @echo off
 
 rem default kill java.exe
+rem usage:
+rem       dkj   -- kill java.exe of which the windowtitle not equqls clojure 1.3.0(for reversing cljr swingrepl)
+rem       dkj file-param    -- same as kjf-pid-ne file-param
 
 if "x%1"=="x" (
   kjtne clojure 1.3.0
